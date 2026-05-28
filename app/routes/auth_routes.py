51a7beb20ffecc438d7ad24ce2e435b5de2327d6
Pah <paahevangelista@gmail.com>
2026-05-28 10:38:59 -0300

    mensagem

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -17,3 +17,72 @@ from app.auth.security import (
 )
 
 from app.auth.jwt_handler import criar_token
+
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Auth"]
+)
+
+
+# LOGIN
+@router.post("/login")
+def login(
+    dados: UsuarioLogin,
+    db: Session = Depends(get_db)
+):
+
+    usuario = db.query(Usuario).filter(
+        Usuario.email == dados.email
+    ).first()
+
+    if not usuario:
+
+        raise HTTPException(
+            status_code=401,
+            detail="Credenciais inválidas"
+        )
+
+    senha_valida = verificar_senha(
+        dados.senha,
+        usuario.senha
+    )
+
+    if not senha_valida:
+
+        raise HTTPException(
+            status_code=401,
+            detail="Credenciais inválidas"
+        )
+
+    token = criar_token({
+
+        "id": usuario.id,
+
+        "email": usuario.email,
+
+        "tipo_usuario": usuario.tipo_usuario
+
+    })
+
+    return {
+
+        "access_token": token,
+
+        "token_type": "bearer",
+
+        "user": {
+
+            "id": usuario.id,
+
+            "nome": usuario.nome,
+
+            "email": usuario.email,
+
+            "tipo_usuario": usuario.tipo_usuario,
+
+            "foto_perfil": usuario.foto_perfil
+
+        }
+
+    }
